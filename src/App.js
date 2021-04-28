2842602ea42567ae017818b13af9a7e8437637b3
AgomohC <chinaemerem98@yahoo.com>
2021-04-28 19:15:20 +0100

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-dark height">
+      <div className="container">
+        <div className="row">
+          <div className="col text-center">
+            <h2 className="text-light text-capitalize my-5">
+              markdown previewer
+            </h2>
+          </div>
+        </div>
+        <section>
+          <div className="row">
+            <div className="col-9 mx-auto">
+              <textarea
+                className="w-100 mb-5 height-30 rounded"
+                value={input}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <div className="col-12 mx-auto">
+              <textarea className="w-100 height-50 mb-5 text-light bg-secondary  rounded"></textarea>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
